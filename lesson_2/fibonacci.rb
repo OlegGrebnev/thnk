c8b48bb1d78d8186675b02dbae8898341797a544
grebnevon <grebnevon@gmail.com>
2019-09-16 13:05:15 +0300

    fibonacci task correction

--- lesson_2/fibonacci.rb
+++ lesson_2/fibonacci.rb
@@ -1,12 +1,10 @@
 #задание 3
-arr = (0..100).to_a
-fibonacci = []
-arr.each do |n|
-  if n < 2
-    fibonacci << arr[n]
-  elsif (fibonacci[-1] + fibonacci[-2]) <= 100
-    fibonacci << fibonacci[-1] + fibonacci[-2]
-  end
+f_value = 0
+fibonacci = [0, 1]
+while f_value <= 100
+  f_value = fibonacci[-1] + fibonacci[-2]
+  break if f_value > 100
+  fibonacci << f_value
 end
 
 puts fibonacci
