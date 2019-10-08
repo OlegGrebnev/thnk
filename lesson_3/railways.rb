ce3d211e6ede8eeeaa96998f4b6ad794f6e76a43
grebnevon <grebnevon@gmail.com>
2019-10-09 01:57:18 +0300

    correct instance variable lesson_3

--- lesson_3/railways.rb
+++ lesson_3/railways.rb
@@ -1,13 +1,15 @@
 class Station
-  attr_reader :station_name, :trains
+  attr_reader :name, :trains
 
-  def initialize(station_name)
-    @station_name = station_name
+  def initialize(name)
+    @name = name
     @trains = []
   end
+
   def arrive(train)
     @trains << train
   end
+
   def departure(train)
     @trains.delete(train)
   end
@@ -15,43 +17,51 @@ end
 
 class Route
   attr_reader :stations
+
   def initialize(from_station, to_station)
     @stations = [from_station, to_station]
   end
+
   def add_station(station)
     @stations.insert(-2, station)
   end
+
   def delete_station(station)
     @stations.delete(station)
   end
 end
 
 class Train
-  attr_reader :train_type, :train_number, :train_car, :train_route, :station
-  attr_accessor :train_speed
+  attr_reader :type, :number, :car, :route, :station
+  attr_accessor :speed
 
-  def initialize(train_number, train_type, train_car)
-    @train_number = train_number
-    @train_type = train_type
-    @train_speed = 0
-    @train_car = train_car
+  def initialize(number, type, car)
+    @number = number
+    @type = type
+    @speed = 0
+    @car = car
   end
+
   def train_stop
-    @train_speed = 0
+    @speed = 0
   end
+
   def add_train_car
-    @train_car += 1 if @train_speed == 0
+    @car += 1 if @speed == 0
   end
+
   def delete_train_car
-    @train_car -= 1 if @train_speed == 0 && @train_car > 0
+    @car -= 1 if @speed == 0 && @car > 0
   end
+
   def get_route(route)
-    @train_route = route
+    @route = route
 #Перемещение на первую станцию в маршруте
     station = route.stations.first
     station.arrive(self)
     @current_station = station
   end
+
   def go_to_station(station)
     if [next_station, prev_station].include?(station)
       @current_station.departure(self)
@@ -59,25 +69,29 @@ class Train
       @current_station = station
     end
   end
+
   def go_to_next_station
     go_to_station(next_station)
   end
+
   def go_to_prev_station
     go_to_station(prev_station)
   end
+
   def next_station
-    current_station_idx = @train_route.stations.index(@current_station)
-    @train_route.stations[current_station_idx + 1] if current_station_idx < @train_route.stations.length - 1
+    current_station_idx = @route.stations.index(@current_station)
+    @route.stations[current_station_idx + 1] if current_station_idx < @route.stations.length - 1
   end
+
   def prev_station
-    current_station_idx = @train_route.stations.index(@current_station)
-    @train_route.stations[current_station_idx - 1] if current_station_idx > 0
+    current_station_idx = @route.stations.index(@current_station)
+    @route.stations[current_station_idx - 1] if current_station_idx > 0
   end
 end
 
 train1 = Train.new(123, "cargo", 56)
-puts train1.train_speed = 1
-puts train1.train_car
+puts train1.speed = 1
+puts train1.add_train_car
 puts train1.train_stop
 train1.add_train_car
 train1.delete_train_car
